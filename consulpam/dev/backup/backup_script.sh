41a518d4522eab1d3536e45efc2853fc4c2d9187
lucas <lucassidpaula@gmail.com>
2025-05-20 18:01:50 -0300

    refactor:changed extention

--- consulpam/dev/backup/backup_script.sh
+++ consulpam/dev/backup/backup_script.sh
@@ -4,4 +4,4 @@ BACKUP_FILE="/backups/backup_${DATE}.sql"
 
 PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER -d $DB_NAME > $BACKUP_FILE
 gzip $BACKUP_FILE
-ls -t /backups/backup_*.sql.gz | tail -n +4 | xargs rm -f
+ls -t /backups/backup_*.sql | tail -n +4 | xargs rm -f
